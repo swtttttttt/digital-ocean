ced4344f463e31d9e596041a7dd0641039f3ff17
swtttttttt <sa.wittenburg@gmail.com>
2022-06-17 12:22:00 -0400

    reacct update contd

---
+++ hulu-page/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "./Components/Header.js"
+import HuluPage from "./Components/HuluPage.js"
+
+function App() {
+
+  return (
+      <div className="state">
+            <div>
+            <Header />
+            <HuluPage />
+        </div>
+      </div>
+  )
+}
+
+export default App;
---
+++ technical-page/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "./Components/Header.js"
+import MuscleContractionMechanism from "./Components/MuscleContractionMechanism.js"
+
+function App() {
+
+  return (
+      <div className="state">
+            <div>
+            <Header />
+            <MuscleContractionMechanism />
+        </div>
+      </div>
+  )
+}
+
+export default App;
